7b5b1bdb6cdb13416e1454eb5a03182b0ad1479e
Timur Potapchuk <timkrasava97@gmai.com>
2022-07-13 02:10:01 +0300

    13.07.22homework

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,62 @@
+﻿void zadacha1()
+{
+Console.WriteLine("введите значение 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+  if (number1>number2)
+    Console.WriteLine("значение " + number1 + " > значения " + number2);
+  else if (number1<number2)
+   Console.WriteLine("значение " + number2 + " > значения " + number1); 
+   else 
+   Console.WriteLine("значения равны"); 
+}
+
+void zadacha2()
+{
+Console.WriteLine("введите три числа поочередно через Enter");
+int numbers = Convert.ToInt32(Console.ReadLine()) ;
+int numbers2 = Convert.ToInt32(Console.ReadLine()) ;
+int numbers3 = Convert.ToInt32(Console.ReadLine()) ;
+int max = numbers;
+if (numbers2 > max)
+
+     max = numbers2;
+// else по началу писал сюда и не мог понять почему не работает. долго же думал
+ if (numbers3 > max)
+
+     max = numbers3;
+
+Console.WriteLine(max);
+}
+
+void zadacha3()
+{
+Console.WriteLine("введите число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int sum = numbers % 2;
+ if (sum<1)
+  Console.WriteLine("четное число");
+ else 
+ Console.WriteLine("нечетное число"); 
+}
+void zadacha4 ()
+{
+Console.WriteLine("введите число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int curr = 1;
+while (curr <= numbers)
+{
+if (curr % 2 == 0)    
+Console.WriteLine(curr);
+curr++;
+}
+}  
+
+
+
+
+// zadacha1();
+// zadacha2();
+// zadacha3();
+// zadacha4();
--- семинар1/Program.cs
+++ семинар1/Program.cs
@@ -46,3 +46,83 @@ else if (day == 2)
  }
  
  quandrant();  // это вызов процедуры
+ 
+
+ /*
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.WriteLine(number);             // задача 
+int once = number % 10;
+int tens = number / 100;
+Console.WriteLine(tens*10+once);*/
+
+/*
+Console.WriteLine("введите значение 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int ost = number2%number1;
+if (ost == 0)
+ Console.WriteLine(number1 + " " + number2 + "кратно");
+ else
+ Console.WriteLine(number1 + " " + number2 + " не кратно, остаток " + ost);*/
+
+
+
+
+        Console.WriteLine("введите значение 1");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите значение 2");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        if (number1 == number2 * number2 || number2 == number1 * number1);
+           Console.WriteLine("является квадратом");
+        else
+           Console.WriteLine(" не является квадратом");
+
+
+
+
+/*
+void ex13()
+{
+    Console.WriteLine("Введите число");
+    int a = Convert.ToInt32(Console.ReadLine());    
+
+    int hungs=(a/100)%10;
+    if (hungs==0) Console.WriteLine("Третьей цифры нет");
+    else 
+    {
+        Console.WriteLine("Третья цифра:");
+        Console.WriteLine(hungs);
+    }
+}
+
+void ex15()
+{
+    Console.WriteLine("Введите число");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    if (a==1)
+        Console.WriteLine("Понедельник");
+    else if (a==2)
+        Console.WriteLine("Вторник");
+    else if (a==3)
+        Console.WriteLine("Среда");
+    else if (a==4)
+        Console.WriteLine("Четверг");
+    else if (a==5)
+        Console.WriteLine("Пятница");
+    else if (a==6)
+        Console.WriteLine("Суббота");
+    else if (a==7)
+        Console.WriteLine("Воскресенье");
+    else
+        Console.WriteLine("Не день недели");
+
+    if (a>5 && a<8) Console.WriteLine("Выходной");
+    else if (a<8) Console.WriteLine("Будний");
+}
+
+*/
+   
